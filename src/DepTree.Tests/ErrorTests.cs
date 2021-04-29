163b43cc45e1ae322e87ddb2c47fb0447fdfe3a7
Maisie <maisiesadler@hotmail.com>
2021-04-29 19:22:04 +0100

    child with error

--- src/DepTree.Tests/ErrorTests.cs
+++ src/DepTree.Tests/ErrorTests.cs
@@ -36,6 +36,30 @@ namespace DepTree.Tests
             Assert.Null(depTree.Children);
         }
 
+        [Fact]
+        public void ExampleWithErrorDepHasChildWithError()
+        {
+            var assembly = this.GetType().Assembly;
+            var fullTypeName = "DepTree.Tests.ErrorTests+ExampleWithErrorDep";
+
+            var depTree = DependencyTree.Create(assembly, fullTypeName);
+
+            Assert.NotNull(depTree);
+            Assert.Equal("root", depTree.Name);
+            Assert.Equal("DepTree.Tests.ErrorTests+ExampleWithErrorDep", depTree.Type.FullName);
+            Assert.Null(depTree.Error);
+            var childdep = Assert.Single(depTree.Children);
+
+            Assert.Equal("tooMany", childdep.Name);
+            Assert.Equal("DepTree.Tests.ErrorTests+ExampleTypeTooMany", childdep.Type.FullName);
+            Assert.Equal(DependencyTreeError.IncorrectConstructors, childdep.Error);
+        }
+
+        public class ExampleWithErrorDep
+        {
+            public ExampleWithErrorDep(ExampleTypeTooMany tooMany) { }
+        }
+
         public class ExampleTypeTooMany
         {
             public ExampleTypeTooMany(ExampleType example) { }
